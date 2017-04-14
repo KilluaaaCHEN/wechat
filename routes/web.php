e2d6ec8d8ca621d02f97d6fd8dd09d31f4b57829
KilluaChen <larry_e@163.com>
2017-04-14 18:32:41 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('http://larry666.com:8080');
+});
+
+
+Route::any('callback', ['as' => 'wechat_callback', 'uses' => 'Wechat\IndexController@callback']);
+Route::get('menu', 'Wechat\IndexController@menu');
+Route::get('notice', 'Wechat\IndexController@notice');
+Route::get('reply', 'Wechat\IndexController@reply');
+Route::get('qrcode', 'Wechat\IndexController@qrCode');
+
+Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
+    Route::get('/user', function () {
+        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+        dd($user);
+    });
+});
